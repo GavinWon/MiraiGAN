973ea0a1251ebc2dc604a7973d5bd1ba30fe62ec
Gavin <gavinmwong@gmail.com>
2020-08-16 17:44:52 -0700

    update GAn model

--- GAN_Model.py
+++ GAN_Model.py
@@ -10,7 +10,8 @@ from tensorflow.keras.layers import Input, Reshape, Dropout, Dense
 from tensorflow.keras.layers import Flatten, BatchNormalization
 from tensorflow.keras.layers import Activation, ZeroPadding2D
 from tensorflow.keras.layers import LeakyReLU
-from tensorflow.keras.layers import UpSampling2D, Conv2D
+from tensorflow.keras.layers import UpSampling2D, Conv1D
+from tensorflow.keras.layers import GlobalAveragePooling1D
 from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.optimizers import Adam
 import numpy as np
@@ -18,6 +19,13 @@ import os
 import time
 import matplotlib.pyplot as plt
 tf.test.gpu_device_name()
+
+##CNN
+# model = Sequential()
+# model.add(Conv1D(filters= 64, kernel_size=3, activation ='relu',strides = 2, padding = 'valid', input_shape= (1000, 1)))
+# model.add(MaxPooling1D(pool_size=2))
+# model.add(Conv1D(filters= 128, kernel_size=3, activation ='relu',strides = 2, padding = 'valid'))
+# model.add(MaxPooling1D(pool_size=2))
 
 def build_generator(seed_size, channels):
     model = Sequential()
@@ -57,31 +65,31 @@ def build_generator(seed_size, channels):
 def build_discriminator():
     model = Sequential()
 
-    model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=image_shape, 
-                     padding="same"))
+    model.add(Conv1D(32, kernel_size=3, strides=2, input_shape= (1000, 1), padding="valid")) #padding=same
     model.add(LeakyReLU(alpha=0.2))
 
     model.add(Dropout(0.25))
-    model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
-    model.add(ZeroPadding2D(padding=((0,1),(0,1))))
+    model.add(Conv1D(64, kernel_size=3, strides=2, padding="valid")) #padding=same
+    # model.add(ZeroPadding2D(padding=((0,1),(0,1))))
     model.add(BatchNormalization(momentum=0.8))
     model.add(LeakyReLU(alpha=0.2))
 
     model.add(Dropout(0.25))
-    model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+    model.add(Conv1D(128, kernel_size=3, strides=2, padding="valid")) #padding=same
     model.add(BatchNormalization(momentum=0.8))
     model.add(LeakyReLU(alpha=0.2))
 
     model.add(Dropout(0.25))
-    model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+    model.add(Conv1D(256, kernel_size=3, strides=1, padding="valid")) #padding=same
     model.add(BatchNormalization(momentum=0.8))
     model.add(LeakyReLU(alpha=0.2))
 
     model.add(Dropout(0.25))
-    model.add(Conv2D(512, kernel_size=3, strides=1, padding="same"))
+    model.add(Conv1D(512, kernel_size=3, strides=1, padding="same")) #padding=same
     model.add(BatchNormalization(momentum=0.8))
     model.add(LeakyReLU(alpha=0.2))
 
+    model.add(GlobalAveragePooling1D())
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(1, activation='sigmoid'))
