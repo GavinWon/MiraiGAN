55f62cebcf40f90d39ada4c03da025e6bead77e3
Gavin <gavinmwong@gmail.com>
2020-08-24 00:38:36 -0700

    added loss function

--- GAN_Model.py
+++ GAN_Model.py
@@ -10,8 +10,7 @@ from tensorflow.keras.layers import Input, Reshape, Dropout, Dense
 from tensorflow.keras.layers import Flatten, BatchNormalization
 from tensorflow.keras.layers import Activation, ZeroPadding2D
 from tensorflow.keras.layers import LeakyReLU
-from tensorflow.keras.layers import UpSampling2D, Conv1D
-from tensorflow.keras.layers import GlobalAveragePooling1D
+from tensorflow.keras.layers import Conv1D, Conv2DTranspose, GlobalAveragePooling1D
 from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.optimizers import Adam
 import numpy as np
@@ -20,6 +19,9 @@ import time
 import matplotlib.pyplot as plt
 tf.test.gpu_device_name()
 
+# This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
 ##CNN
 # model = Sequential()
 # model.add(Conv1D(filters= 64, kernel_size=3, activation ='relu',strides = 2, padding = 'valid', input_shape= (1000, 1)))
@@ -27,40 +29,43 @@ tf.test.gpu_device_name()
 # model.add(Conv1D(filters= 128, kernel_size=3, activation ='relu',strides = 2, padding = 'valid'))
 # model.add(MaxPooling1D(pool_size=2))
 
-def build_generator(seed_size, channels):
+def build_generator(z):
     model = Sequential()
 
-    model.add(Dense(4*4*256,activation="relu",input_dim=seed_size))
-    model.add(Reshape((4,4,256)))
+    model.add(Dense(128, inputs = z, activation="relu"))
 
-    model.add(UpSampling2D())
-    model.add(Conv2D(256,kernel_size=3,padding="same"))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
+    model.add(Conv2DTranspose(512,kernel_size=3,padding="same")) #padding=same
+    model.add(BatchNormalization()) #momentum=0.8
+    model.add(LeakyReLU())
 
-    model.add(UpSampling2D())
-    model.add(Conv2D(256,kernel_size=3,padding="same"))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
+    model.add(Conv2DTranspose(256,kernel_size=3,padding="same")) #padding=same
+    model.add(BatchNormalization()) #momentum=0.8
+    model.add(LeakyReLU())
+    
+    model.add(Conv2DTranspose(128, kernel_size=3, strides=2, padding="valid")) #padding=same
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+    
+    model.add(Conv2DTranspose(64, kernel_size=3, strides=2, padding="valid")) #padding=same
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+    
+    model.add(Dense(115))
+    
    
     # Output resolution, additional upsampling
-    model.add(UpSampling2D())
-    model.add(Conv2D(128,kernel_size=3,padding="same"))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
+    # model.add(Conv2D(128,kernel_size=3,padding="same")) #padding=same
+    # model.add(BatchNormalization(momentum=0.8))
+    # model.add(LeakyReLU()
 
-    if GENERATE_RES>1:
-      model.add(UpSampling2D(size=(GENERATE_RES,GENERATE_RES)))
-      model.add(Conv2D(128,kernel_size=3,padding="same"))
-      model.add(BatchNormalization(momentum=0.8))
-      model.add(Activation("relu"))
 
     # Final CNN layer
-    model.add(Conv2D(channels,kernel_size=3,padding="same"))
-    model.add(Activation("tanh"))
+    # model.add(Activation("tanh"))
 
     return model
 
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
 
 def build_discriminator():
     model = Sequential()
@@ -71,22 +76,22 @@ def build_discriminator():
     model.add(Dropout(0.25))
     model.add(Conv1D(64, kernel_size=3, strides=2, padding="valid")) #padding=same
     # model.add(ZeroPadding2D(padding=((0,1),(0,1))))
-    model.add(BatchNormalization(momentum=0.8))
+    model.add(BatchNormalization())
     model.add(LeakyReLU(alpha=0.2))
 
     model.add(Dropout(0.25))
     model.add(Conv1D(128, kernel_size=3, strides=2, padding="valid")) #padding=same
-    model.add(BatchNormalization(momentum=0.8))
+    model.add(BatchNormalization())
     model.add(LeakyReLU(alpha=0.2))
 
     model.add(Dropout(0.25))
     model.add(Conv1D(256, kernel_size=3, strides=1, padding="valid")) #padding=same
-    model.add(BatchNormalization(momentum=0.8))
+    model.add(BatchNormalization())
     model.add(LeakyReLU(alpha=0.2))
 
     model.add(Dropout(0.25))
     model.add(Conv1D(512, kernel_size=3, strides=1, padding="same")) #padding=same
-    model.add(BatchNormalization(momentum=0.8))
+    model.add(BatchNormalization())
     model.add(LeakyReLU(alpha=0.2))
 
     model.add(GlobalAveragePooling1D())
@@ -95,6 +100,37 @@ def build_discriminator():
     model.add(Dense(1, activation='sigmoid'))
 
     return model
+
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+#plot the accuracy and the validation accuracy
+def plot_acc(h):
+    plt.plot(h.history['accuracy'])
+    plt.plot(h.history['val_accuracy'])
+
+    plt.title('model accuracy')
+    plt.ylabel('accuracy and loss')
+    plt.xlabel('epoch')
+
+    plt.legend(['acc', 'val acc' ], loc='upper left')
+    plt.show()
+    
+#plot the loss and validation loss
+def plot_loss(h):
+    plt.plot(h.history['loss'])
+    plt.plot(h.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('accuracy and loss')
+    plt.xlabel('epoch')
+
+    plt.legend(['loss', 'val loss' ], loc='upper left')
+    plt.show()
 
 def printabc():
     print("Hi")
