d36812d95f8e4d03a088629a2bc0e50401ff9414
Gavin <gavinmwong@gmail.com>
2020-09-02 23:13:54 -0700

    added remove Col

--- log-convert-csv.py
+++ log-convert-csv.py
@@ -7,17 +7,12 @@ Created on Tue Sep  1 21:47:51 2020
 
 import csv
 
-
-with open('conn34.csv',"w") as outfile:
-    for log_record in ParseZeekLogs("conn34.log.labeled", output_format="csv", safe_headers=False, fields=["ts","id.orig_h","id.orig_p","id.resp_h","id.resp_p"]):
-        if log_record is not None:
-            outfile.write(log_record + "\n")
-            
-with open("connlabel_malicious.csv", 'w', newline='') as file:  
-    with open("connlabel_benign.csv", 'w', newline='') as file2:         
+def covertLogToCSV():
+    with open("connlabel34.csv", 'w', newline='') as file:  
+        # with open("connlabel_benign.csv", 'w', newline='') as file2:         
         with open("conn34.log.labeled", "r") as readfile:
             csv_writer_m = csv.writer(file)
-            csv_writer_b = csv.writer(file2)
+            # csv_writer_b = csv.writer(file2)
             
             for i in range(6):
                 readfile.readline()
@@ -25,23 +20,34 @@ with open("connlabel_malicious.csv", 'w', newline='') as file:
             l = readfile.readline().split()
             l = l[1:]
             csv_writer_m.writerow(l)
-            csv_writer_b.writerow(l)
+            # csv_writer_b.writerow(l)
             
             readfile.readline()
             
             Lines = readfile.readlines()
-            
+            count = 0
             for line in Lines:
-                if (len(l) == 1):
+                l = line.split()
+                if (count % 1000000 == 0):
+                    print(count)
+                if (len(l) < 21):
                     continue
-                l = line.split()[1:]
-                print(l)
-                if (l[20] == "Benign"): #data sample is benign
-                     csv_writer_b.writerow(l)
-                else: #data sample is malicious
-                    csv_writer_m.writerow(l)
+                csv_writer_m.writerow(l)
+                # if (l[21] == "Benign"): #data sample is benign
+                #      csv_writer_b.writerow(l)
+                # else: #data sample is malicious
+                #     csv_writer_m.writerow(l)
+                count += 1
             
-           
+
+def removeCol():
+    with open("connlabel_benign.csv","r") as source:
+        rdr = csv.reader( source )
+        with open("connlabel_benign_new.csv","w") as result:
+            wtr = csv.writer( result )
+            for r in rdr:
+                wtr.writerow( (r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[11], r[14], r[15], r[16], r[17], r[18],  r[19], r[21], r[22] )   )  
+     
     
 print(l.shape)
 
