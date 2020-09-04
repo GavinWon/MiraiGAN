ffa44077e675629cbd70e6bf0e1e31fa378c9b16
Gavin <gavinmwong@gmail.com>
2020-09-03 22:41:33 -0700

    edit

--- log-convert-csv.py
+++ log-convert-csv.py
@@ -6,6 +6,8 @@ Created on Tue Sep  1 21:47:51 2020
 """
 
 import csv
+import pandas
+import fileinput
 
 def covertLogToCSV():
     with open("connlabel34.csv", 'w', newline='') as file:  
@@ -41,14 +43,73 @@ def covertLogToCSV():
             
 
 def removeCol():
-    with open("connlabel_benign.csv","r") as source:
+    with open("connlabel_malicious.csv","r") as source:
         rdr = csv.reader( source )
-        with open("connlabel_benign_new.csv","w") as result:
+        with open("connlabel_malicious_new.csv","w", newline="") as result:
+            count = 0
             wtr = csv.writer( result )
             for r in rdr:
-                wtr.writerow( (r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[11], r[14], r[15], r[16], r[17], r[18],  r[19], r[21], r[22] )   )  
+                if (count % 1000000 == 0):
+                    print(count)
+                wtr.writerow([r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[11], r[14], r[15], r[16], r[17], r[18],  r[19], r[21], r[22] ] )  
+                count += 1
+                
+
      
+
+def removeRows():
+    #remove rows with - in the row
+    with open('connlabel_malicious_new.csv', 'r') as read_obj:
+        with open('connlabel_malicious_new1.csv', 'w', newline='') as write_obj:
+            csv_reader = csv.reader(read_obj)
+            csv_writer = csv.writer(write_obj)
+            count = 0
+            for row in csv_reader:
+                check = row[:-1] #dont consider the last column
+                if ("-" not in check):
+                    csv_writer.writerow(row)
+                    count += 1
+                if (count % 1000000 == 0):
+                    print(count)
+            print(count)
+            
+    #remove duplicates
+    count = 0
+    with open('connlabel_malicious_new1.csv','r') as in_file:
+        with open('connlabel_malicious_new2.csv','w', newline="") as out_file:
+            csv_writer = csv.writer(out_file)
+            seen = set() # set for fast O(1) amortized lookup
+            for line in in_file:
+                line = tuple(line.split(",")) 
+                check = line[2:]
+                if check in seen: continue # skip duplicate
+                seen.add(check)
+                csv_writer.writerow(line)
+                if (count % 1000000 == 0):
+                    print(count)
+                count += 1
+        print(count)    
     
+    
+def createDataSet():
+    with open('connlabel_malicious_new2.csv', 'r') as read_obj:
+        with open('malicious.csv', 'w', newline='') as write_obj:
+            csv_reader = csv.reader(read_obj)
+            csv_writer = csv.writer(write_obj)
+            
+            for i in range(10000000):
+                csv_writer.writerow(next(csv_reader))
+        
+import pandas as pd
+df = pd.read_csv('connlabel_benign_new2.csv')    
+df.head()
+
+db = pd.read_csv('34\\connlabel_benign34_new2.csv')
+            
+        
+        
+        
+        
 print(l.shape)
 
 with open("mirai_testing_labels.csv", 'w', newline='') as file:
