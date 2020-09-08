742711df9241b4fa3681089c38857a3d6fbdc13b
Gavin <gavinmwong@gmail.com>
2020-09-07 23:56:29 -0700

    update gan trainign

--- GANTraining.py
+++ GANTraining.py
@@ -51,13 +51,20 @@ X_train = tf.reshape(X_train, (764137, 115, 1))
 X_test = tf.reshape(X_test, (534388, 115, 1))
 
 
-#Discriminator Load-Up
+##ACTUAL GAN TRAINING
+
+EPOCHS = 50
+SEED_SIZE = 100
+BATCH_SIZE = 16
+
+
+#Models Load Up Load-Up
 json_file = open('pretrain_disc.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 
-model = tf.keras.models.model_from_json(loaded_model_json)
-model.load_weights("pretrain_disc.h5")
+discriminator = tf.keras.models.model_from_json(loaded_model_json)
+discriminator.load_weights("pretrain_disc.h5")
 
 preds_test = model.predict_classes(X_test)
 from sklearn.metrics import accuracy_score
@@ -65,10 +72,9 @@ print("Accuracy = {}".format(accuracy_score(Y_test, preds_test)))
 
 #Generator
 
+generator = build_generator(SEED_SIZE)
 
-EPOCHS = 50
-noise_dim = 100
-num_examples_to_generate = 16
+
 
 generator_optimizer = tf.keras.optimizers.Adam(1.5e-4,0.5)
 discriminator_optimizer = tf.keras.optimizers.Adam(1.5e-4,0.5)
@@ -81,8 +87,8 @@ def train_step(dataset):
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
         generated_dataset = generator(seed)
         
-        real_output = discriminator(images, training=True)
-        fake_output = discriminator(generated_images, training=True)
+        real_output = discriminator(images)
+        fake_output = discriminator(generated_dataset)
         
         gen_loss = generator_loss(fake_output)
         disc_loss = discriminator_loss(real_output, fake_output)
@@ -95,12 +101,35 @@ def train_step(dataset):
         discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
         return gen_loss,disc_loss
     
-    
-x = 5
-x = x + 1
-printabc()
 
-def test():
-    global x
-    print(x + 1)
-test()
+def train(dataset, epochs):
+  fixed_seed = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, SEED_SIZE))
+  start = time.time()
+
+  for epoch in range(epochs):
+    epoch_start = time.time()
+
+    gen_loss_list = []
+    disc_loss_list = []
+
+    for image_batch in dataset:
+      t = train_step(image_batch)
+      gen_loss_list.append(t[0])
+      disc_loss_list.append(t[1])
+
+    g_loss = sum(gen_loss_list) / len(gen_loss_list)
+    d_loss = sum(disc_loss_list) / len(disc_loss_list)
+
+    epoch_elapsed = time.time()-epoch_start
+    print (f'Epoch {epoch+1}, gen loss={g_loss},disc loss={d_loss},'\
+           ' {hms_string(epoch_elapsed)}')
+    save_images(epoch,fixed_seed)
+
+  elapsed = time.time()-start
+  print (f'Training time: {hms_string(elapsed)}')
+  
+
+generator = build_generator(100)
+
+noise = tf.random.normal([1,100])
+generated_image = generator(noise, training=False)
