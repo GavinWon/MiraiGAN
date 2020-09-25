434a8e01d8339a14d37c8576a3abcffd55c8f53c
Gavin <gavinmwong@gmail.com>
2020-09-24 18:25:28 -0700

    minor edit

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -93,13 +93,3 @@ X = X[~np.isnan(X).any(axis=1)]
 Y = Y[:-1]
 
 
-
-
-def CountFrequency(my_list):
-    freq = {}
-    for item in my_list: 
-        if (item in freq): 
-            freq[item] += 1
-        else: 
-            freq[item] = 1
-    return freq
