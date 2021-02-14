9bc247caaaf0c35910a9b7cc663bf32d71efee47
Gavin <gavinmwong@gmail.com>
2021-02-14 14:54:10 -0800

    updated GAN and new plot

--- GAN_Evaluate.py
+++ GAN_Evaluate.py
@@ -52,20 +52,36 @@ def plot_fake_accuracy(array, title):
     plt.savefig('fake.png')
     plt.show()
 
+def plot_real_accuracy(array, title):
+    X = np.arange(len(array)) + 1
+    plt.figure(figsize=(20, 16))
+    
+    plt.plot(X, array, 'ro', markersize = 5)
+    
+    plt.title(title, fontsize=40, pad=30)
+    plt.ylabel('Accuracy', fontsize=30, labelpad=20)
+    plt.yticks(np.arange(0.55, 1.05, 0.05), fontsize=20)
+    plt.xlabel('Steps', fontsize=30, labelpad=20)
+    plt.xticks(np.arange(0, 1600, 100), fontsize=20)
+    
+
+    plt.tight_layout()
+    # set(gca,'xtick',xmin:.1:xmax)
+    plt.savefig('real.png')
+    plt.show()
 
 
 plt.plot(np.arange(len(50)) + 1)
 
-fake_old = np.load('fake_old_accuracy.npy')[2390:]
+fake_old = np.load('Saved Metric Values\\fake_old_accuracy.npy')[2390:]
 fake_new_title = "Fake Data Accuracy"
 plot_gan_accuracy(fake_old, fake_new_title)
 
-fake_new = np.load('fake_new_accuracy.npy')[2391:]
-fake_new_title = "Fake Data Accuracy"
+fake_new = np.load('Saved Metric Values\\fake_new_accuracy.npy')[2391:]
+fake_new_title = "Generated/Fake Data Accuracy"
 plot_fake_accuracy(fake_new, fake_new_title)
 
-real = np.load('real_new_accuracy.npy')[2391:]
+real = np.load('Saved Metric Values\\real_new_accuracy.npy')[2391:]
 real = real[:600]
 real_title = "Real Data Accuracy"
-
-plot_fake_accuracy(real, real_title)
+plot_real_accuracy(real, real_title)
