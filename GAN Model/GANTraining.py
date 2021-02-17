5639091856a560b834787b4bee73b13c14cbcf6e
Gavin <gavinmwong@gmail.com>
2021-02-17 11:21:21 -0800

    added comments/documentation

--- GAN Model/GANTraining.py
+++ GAN Model/GANTraining.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 24 18:35:43 2020
 
-@author: Gavin
 """
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 25 00:37:57 2020
-
 @author: Gavin
 """
 
+'''import statements'''
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.layers import Input, Reshape, Dropout, Dense 
@@ -30,16 +23,17 @@ from numpy.random import randn
 from numpy.random import randint
 
 
-
 import pandas as pd
 import sys
 
 from matplotlib import pyplot
 from tensorflow.keras import backend
 
+'''Import GAN Model'''
 sys.path.append("D:\\Repos\\MiraiGAN")
 from GAN_Model2 import *
 
+'''Summarize the performance of the GAN Model by printing the accuracy'''
 def summarize_performance(step, g_model, d_model, latent_dim, dataset, n_samples=200):
     X_real = dataset[0]
     Y_real = dataset[1]
@@ -71,7 +65,7 @@ def summarize_performance(step, g_model, d_model, latent_dim, dataset, n_samples
 
 
 
-# train the generator and discriminator
+'''Training the generator and discriminator'''
 def train(g_model, d_model, gan_model, data_train, data_test, latent_dim, n_epochs=10, n_batch=200):
 
     # calculate the number of batches per training epoch
@@ -83,6 +77,7 @@ def train(g_model, d_model, gan_model, data_train, data_test, latent_dim, n_epoc
     # calculate the size of half a batch of samples
     half_batch = int(n_batch / 2)
     #print('n_epochs=%d, n_batch=%d, 1/2=%d, b/e=%d, steps=%d' % (n_epochs, n_batch, half_batch, bat_per_epo, n_steps))
+    
     # manually enumerate epochs
     for i in range(n_steps): #n_steps = 1480
         
@@ -112,16 +107,12 @@ def train(g_model, d_model, gan_model, data_train, data_test, latent_dim, n_epoc
         # evaluate the model performance every so often
         # if ((i) % (10) == 0):
         summarize_performance(i, g_model, d_model, latent_dim, data_test) 
-        
-
-        # print("Finished Step:", i, "out of", n_steps)
-        
+                
         
         # # summarize loss on this batch
         # print('>%d, d[%.3f,%.3f], g[%.3f]' % (i+1, d_loss1, d_loss2, g_loss))
         
 
-    #Save model for the discriminator and generator
 
 
 
@@ -134,6 +125,7 @@ fake_old_accuracy = []
 fake_new_accuracy = []
 real_new_accuracy = []
 
+# Saving Acc/Loss values
 np.save('d_loss1_values.npy', d_loss1_values)
 np.save('d_loss2_values.npy', d_loss2_values)
 np.save('g_loss_values.npy', g_loss_values)
@@ -141,73 +133,44 @@ np.save('fake_old_accuracy.npy', fake_old_accuracy)
 np.save('fake_new_accuracy.npy', fake_new_accuracy)
 np.save('real_new_accuracy.npy', real_new_accuracy)
 
-test = np.load('fake_old_accuracy.npy')
 
 # size of the latent space
-latent_dim = 10 #or 4
+latent_dim = 10 
 
-# load the discriminator model
+'''Load the discriminator model from previous training'''
 json_file = open('pretrainV1\\pretrain_disc.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
-
 c_model = tf.keras.models.model_from_json(loaded_model_json)
 c_model.load_weights("pretrainV1\\pretrain_disc.h5")
 
+'''create the discriminator'''
 d_model, _ = build_main_disc()
 
-# create the generator
+'''create the generator'''
 g_model = build_generator(latent_dim)
 
-# create the gan
+'''create the gan'''
 gan_model = define_gan(g_model, d_model)
 
-# load data 1
-X_train = np.load('data_train.npy')
-data_train = [X_train, Y_train]
-Y = np.full(len(X_train), 1)
-X_test = np.load('data_test.npy')
-Y2 = np.full(len(X_test), 1)
-data_test = [X_test, Y_test]
 
-# load data 2
-X_train = np.load('Saved Data\\data_mirai_train.npy')
+'''load data'''
+X_train = np.load('Saved Converted Data (npy)\\data_mirai_train.npy')
 Y_train = np.full(len(X_train), 1)
 data_train = [X_train, Y_train]
-X_test = np.load('Saved Data\\data_mirai_test.npy')
+X_test = np.load('Saved Converted Data (npy)\\data_mirai_test.npy')
 Y_test = np.full(len(X_test), 1)
 data_test = [X_test, Y_test]
 
-# train model
+'''Pretraining discriminator'''
+d_model.fit(X_train, Y_train, validation_data = (X_test, Y_test), epochs = 10, batch_size = 200, shuffle = True)
+
+'''train model'''
 train(g_model, d_model, gan_model, data_train, data_test, latent_dim)
 
 
-##TESTING
-''' 1 '''
-X_real, y_real = get_real_samples(data, 100)
-X_real = tf.reshape(X_real, (100, 30, 1))
-d_loss1 = d_model.train_on_batch(X_real, y_real)
 
-''' 2 '''
-X_fake, y_fake = generate_fake_samples(g_model, latent_dim, 200) #Y_fake is all 0
-d_loss2 = d_model.train_on_batch(X_fake, y_fake)
-
-''' 3 '''
-X_gan, y_gan = generate_latent_points(latent_dim, 200), ones((200, 1)) #Y_gan is all 1
-g_loss = gan_model.train_on_batch(X_gan, y_gan)
-
-
-#EXTRA
-d_loss2 = d_model.train_on_batch(X_fake, y_fake)
-
-X_fake_new, Y_fake_new = generate_fake_samples(g_model, latent_dim)
-preds_fake_new = np.round(d_model.predict(X_fake_new))
-
-print("New Accuracy = {}".format(accuracy_score(Y_fake_new, preds_fake_new)))
-
-d_model.fit(X_train, Y_train, validation_data = (X_test, Y_test), epochs = 10, batch_size = 200, shuffle = True)
-
-#Saving Models
+'''Saving Models'''
 file_name = "d_model" 
 json = file_name + ".json"
 h5 = file_name + ".h5"
@@ -232,7 +195,7 @@ with open(json, "w") as json_file:
     json_file.write(model_json)
     gan_model.save_weights(h5)
 
-#Opening Models
+'''Opening Models'''
 json_file = open('Saved Models\\d_model.json', 'r')
 d_model_json = json_file.read()
 json_file.close()
@@ -252,25 +215,36 @@ gan_model = tf.keras.models.model_from_json(gan_model_json)
 gan_model.load_weights("Saved Models\\gan_model.h5")
     
     
-#Prepare testing set
-    
+'''Prepare testing set'''
 X_fake, Y_fake = generate_fake_samples(g_model, latent_dim, 200)
 X_real, Y_real = get_real_samples(data)
 preds_fake = np.round(d_model.predict(X_fake))
 preds_real = np.round(d_model.predict(X_real))
-
 np.save('X_fake.npy', X_fake)
 
-test = d_model.predict(X_fake)
-test2 = d_model.predict(X_real)
-                      
+
+'''Metrics'''
 from sklearn.metrics import accuracy_score
 print("Gen Accuracy = {}".format(accuracy_score(Y_fake, preds_fake)))
 print("Real Accuracy = {}".format(accuracy_score(Y_real, preds_real)))
 
 
-X_real, Y_real = get_real_samples(data)
-_, acc = d_model.evaluate(X_fake, Y_fake, verbose=0)
-_, acc = c_model.evaluate(X_fake, Y_fake, verbose=0)
 
 
+'''EXTRA/TESTING'''
+#   1    #
+X_real, y_real = get_real_samples(data, 100)
+X_real = tf.reshape(X_real, (100, 30, 1))
+d_loss1 = d_model.train_on_batch(X_real, y_real)
+
+#   2    #
+X_fake, y_fake = generate_fake_samples(g_model, latent_dim, 200) #Y_fake is all 0
+d_loss2 = d_model.train_on_batch(X_fake, y_fake)
+
+#   3    #
+X_gan, y_gan = generate_latent_points(latent_dim, 200), ones((200, 1)) #Y_gan is all 1
+g_loss = gan_model.train_on_batch(X_gan, y_gan)
+
+#   4    #
+X_fake_new, Y_fake_new = generate_fake_samples(g_model, latent_dim)
+preds_fake_new = np.round(d_model.predict(X_fake_new))
--- GAN Model/firstTrain_Disc.py
+++ GAN Model/firstTrain_Disc.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Sep 24 19:25:39 2020
-
 @author: Gavin
 """
 
 
-
+'''import statements'''
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Reshape, Dropout, Dense 
 from tensorflow.keras.layers import Flatten, BatchNormalization
@@ -19,82 +16,61 @@ import numpy as np
 import pandas as pd
 import sys
 
+'''Import GAN Model'''
 sys.path.append("D:\\Repos\\MiraiGAN")
 from GAN_Model2 import *
 
 
 
-
-
-test = np.array(X_train)
-
-test = np.array(X_train)
-print(np.mean(test))
-print(np.amin(test))
-print(np.amax(test))
-print(X.shape)
-
-other = X_other_train
-
+'''Reshaping the main X data'''
 X_train = tf.reshape(X_train, (79794, 1214, 1))
 X_test = tf.reshape(X_test, (26598, 1214, 1))
 
-
+'''Reshapign the sub-datasets IP and other (non-IP)'''
 X_IP_train = tf.reshape(X_IP_train, (79794, 1214, 1))
 X_IP_test = tf.reshape(X_IP_test, (26598, 1214, 1))
 X_other_train = tf.reshape(X_other_train, (79794, 10, 1))
 X_other_test = tf.reshape(X_other_test, (26598, 10, 1))
 
 
-
+'''Initialize the model'''
 _,disc_main = build_main_disc()
-disc_ip = build_disc_ip()
+disc_ip = build_ip_disc()
 disc = build_discriminator(disc_ip, disc_main)
 
-
+'''Training discriminator to classify benign/malware'''
 history = disc.fit([X_IP_train, X_other_train], Y_train, epochs = 5, batch_size = 100, validation_data = ([X_IP_test, X_other_test], Y_test), shuffle = True)
 
 
 
 
-
-
-# Preparing new Data
-test = X_IP_train[0]
-test = tf.reshape(test, (1, 1214, 1))
-wubdub = disc_ip.predict([test])
-wubdub2 = disc_ip.predict([test])
-wubdub3 = disc_ip.predict([test])
+'''Saving converted data file for GAN Training'''
+other = X_other_train
 IP = disc_ip.predict(X_IP_train)
-
 final = np.hstack((IP, other)) #1-20 --> IP, #21 - 30 --> other
-
 np.save('data.npy', final)
 
+'''Testing discriminator prediction'''
+test = X_IP_train[0]
+test = tf.reshape(test, (1, 1214, 1))
+predict = disc_ip.predict([test])
 
 
-
-
-
-#Saving disc model
+'''Saving discriminator model'''
 file_name = "pretrain_disc"
-    
 json = file_name + ".json"
-h5 = file_name + ".h5"
-    
+h5 = file_name + ".h5"  
 model_json = disc.to_json()
 with open(json, "w") as json_file:
     json_file.write(model_json)
     disc.save_weights(h5)
 
-#Metrics
-
+'''Metrics'''
 from sklearn.metrics import accuracy_score
 preds_training = np.round(disc.predict([X_IP_train, X_other_train]))
 preds_test = np.round(disc.predict([X_IP_test, X_other_test]))
 print("Accuracy = {}".format(accuracy_score(Y_train, preds_training)))
 print("Accuracy = {}".format(accuracy_score(Y_test, preds_test)))
-
 
 from sklearn.metrics import confusion_matrix
 print(confusion_matrix(Y_test, preds_test))
@@ -102,4 +78,5 @@ print(confusion_matrix(Y_train, preds_training))
 plot_acc(history)
 plot_loss(history)
 
+'''Model summary'''
 print(disc.summary())
